13912f24514c72365ff897bca56481375f5bedef
gcollins1317 <gracecollins1000@gmail.com>
2018-07-13 15:01:09 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,9 +1,11 @@
 class GardenGnome
-  attr_reader :personality 
+  attr_reader :personality, :hat_color 
   attr_accessor :name, :age, :gluten_allergy
   
-  def initialize(personality = "evil")
+  def initialize(personality = "evil", hat_color = "red")
     @personality = personality  
+    @hat_color = hat_color
   end 
+
     
 end 
