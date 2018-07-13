e1ae8468b75c8840ab03570322216e191ec76cde
gcollins1317 <gracecollins1000@gmail.com>
2018-07-13 15:04:02 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -9,6 +9,12 @@ class GardenGnome
   def gnaw 
     "Gnawing on a tree!!!"
   end 
+  def shout 
+    "GNARLY!!!"
+  end 
+  def introduce_self 
+    "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+  end 
 
     
 end 
